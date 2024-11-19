5fde296e8cbc31df21c6009151f129250b0dc5a9
Bruno A. Rolim <brolim@gmail.com>
2024-11-19 17:12:17 -0300

    feat(search): naive way is working

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -3,10 +3,21 @@ require_relative "../src/search"
 describe "search an element inside an array" do
   subject { index_of(array, target) }
 
-  let(:array) { [1, 2] }
-  let(:target) { 1 }
+  context "when the target exists in the array" do
+    let(:array) { [1, 2] }
+    let(:target) { 1 }
 
-  it "pass" do
-    expect(subject).to eq(nil)
+    it "responds 0" do
+      expect(subject).to eq(0)
+    end
+  end
+
+  context "when the target does not exist in the array" do
+    let(:array) { [1, 2] }
+    let(:target) { 3 }
+
+    it "responds nil" do
+      expect(subject).to eq(nil)
+    end
   end
 end
--- src/search.rb
+++ src/search.rb
@@ -1,3 +1,9 @@
 def index_of array, target
+  array.each_with_index do |el, i|
+    if el == target
+      return i
+    end
+  end
+
   nil
 end
